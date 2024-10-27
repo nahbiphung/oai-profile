0f12f3892668da0fe833ddb5ee99725777da06d7
Phung Gia Oai <phunggiaoai@gmail.com>
2024-10-27 23:11:50 +0700

    Main Profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, NgClass],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'oai-profile';
+    currentSection: 'introduction' | 'about-me' | 'skill-set' = 'introduction';
+
+    scroll(el: HTMLElement) {
+        el.scrollIntoView({ behavior: 'smooth' });
+    }
 }
