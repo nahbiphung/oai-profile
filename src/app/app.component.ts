a7b0c44a3013e9a977f3a37d4d7d93045c6b8084
APAC\POH5HC <Oai.PhungGia@vn.bosch.com>
2024-10-30 19:14:29 +0700

    change to click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { NgClass } from '@angular/common';
+import { NgClass, NgStyle, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -7,12 +7,19 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, NgClass, MatProgressBarModule],
+    imports: [
+        RouterOutlet,
+        NgClass,
+        NgStyle,
+        UpperCasePipe,
+        MatProgressBarModule,
+    ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
 export class AppComponent {
-    currentSection: 'introduction' | 'about-me' | 'skill-set' = 'introduction';
+    currentSection: 'introduction' | 'about-me' | 'skills' = 'introduction';
+    isFlippedCard = false;
 
     scroll(el: HTMLElement): void {
         el.scrollIntoView({ behavior: 'smooth' });
